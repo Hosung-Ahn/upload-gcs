a1ffec74c6e523b54a4649eba3a4a33a58a4c4b9
Hosung <an3735297@naver.com>
2024-01-11 11:19:52 +0900

    프로젝트 생성, 깃이그노어 수정

---
+++ upload-gcs/src/main/java/com/example/uploadgcs/UploadGcsApplication.java
@@ -0,0 +1,13 @@
+package com.example.uploadgcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadGcsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadGcsApplication.class, args);
+    }
+
+}
---
+++ upload-gcs/src/test/java/com/example/uploadgcs/UploadGcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uploadgcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadGcsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
